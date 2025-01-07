0c8f7bea4be75cacf33c52cdc3f56a0528baf540
Atharva Naik <138719165+AtharvaGirishNaik@users.noreply.github.com>
2025-01-08 00:03:19 +0530

    Update minimum-moves-to-equal-array-elements-ii.java

--- Microsoft/minimum-moves-to-equal-array-elements-ii.java
+++ Microsoft/minimum-moves-to-equal-array-elements-ii.java
@@ -1,5 +1,13 @@
+// Approach 1: Sorting the array
 
-https://leetcode.com/problems/minimum-moves-to-equal-array-elements-ii/solutions/2215782/visual-explanation-interview-tips-java
+// This is fairly straight forward. If we simply sort the nums array, we can obtain the median by simply accessing the middle element. This allows us to find the median in O(nlogn) time. Keep in mind that if the array is of even length, we can just pick either of the two middle values as our median.
+
+// Approach 2: Quick Select
+
+// The sorting method is inefficient because it sorts the entire array. All we really need to do is put the median element where it's supposed to be; we don't care about where the other elements end up. Sound familiar? Well turns out we can do exactly that using quick select. In quick select, we'll just select the middle element; n/2 and retrieve the value at that position. This allows us to find the median element in O(n) time on average.
+
+// If you're unfamiliar with quick select, I highly recommend reading this guide that I wrote which explains the entire process in detail, including explanations for time complexity.
+// https://leetcode.com/problems/minimum-moves-to-equal-array-elements-ii/solutions/2215782/visual-explanation-interview-tips-java
 
 class Solution {
     public int minMoves2(int[] nums) {
